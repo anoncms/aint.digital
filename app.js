ed0cebf6507aef48685571c5d0dbb1733be5a6c2
Pragmatic <cryptopragmatic@protonmail.com>
2022-11-01 23:54:45 +0100

    Updates

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ function createEl(i) {
   const hue = Math.round(360 / numberOfEls * i);
   el.classList.add('el');
 //   el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
-  el.style.backgroundColor = '#ff0000';
+  el.style.backgroundColor = '#C90000';
   el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)';
   tl.add({
     begin: function() {
       anime({
         targets: el,
         // backgroundColor: ['hsl(' + hue + ', 40%, 60%)', 'hsl(' + hue + ', 60%, 80%)'],
-        backgroundColor: '#ff0000',
+        backgroundColor: '#C90000',
         rotate: [rotate + 'deg', rotate + 10 +'deg'],
         translateY: [translateY + '%', translateY + 10 + '%'],
         scale: [1, 1.25],
